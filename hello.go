4f37256cd1557a551dcf89bf2815cc8b5a963755
unknown <svon>
2018-03-21 22:48:52 +0800

    for & switch

--- hello.go
+++ hello.go
@@ -2,6 +2,17 @@ package main
 
 import (
 	fmt "fmt"
+  "strconv"
+  "reflect"
+)
+
+const (
+  B float64 = 1 << (iota * 10)
+  KB
+  MB
+  GB
+  TB
+  PB
 )
 
 func main() {
@@ -17,4 +28,42 @@ func main() {
 
 	age := 22 //声明变量的简写格式
 	fmt.Println("简写", age)
+
+
+  a := 65
+  stra := strconv.Itoa(a)  //将数字转为字符串
+  fmt.Println(stra, reflect.TypeOf(stra))
+
+  b, _ := strconv.Atoi(stra)  //字符串转为数字
+  fmt.Println(b, reflect.TypeOf(b))
+
+  fmt.Println(B, KB, GB, PB)
+
+
+  for i, len := 1, 10; i < len; i++ {
+    fmt.Println("i : ", i)
+  }
+
+  i := 1
+  for true {
+    fmt.Println("i : ", i)
+    i++
+    if i > 10 {
+      break
+    }
+  }
+
+  c := 5
+  switch true {
+  case c > 0:
+    fmt.Println("c > 0")
+    fallthrough //继续检查符合条件的语句
+  case c > 1:
+    fmt.Println("c > 1")
+  case c > 2:
+    fmt.Println("c > 2")
+  default:
+    fmt.Println("default")
+  }
+
 }
